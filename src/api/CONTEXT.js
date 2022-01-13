04ff2990f12f9ff9c4352ee57282eb8304d4b706
Aldodev01 <aldodevox@gmail.com>
2022-01-13 06:45:21 -0800

    uplaad aldo

--- src/api/CONTEXT.js
+++ src/api/CONTEXT.js
@@ -1,9 +1,4 @@
 import axios from "axios";
-import {
-  API_BASEURL_DEV,
-  API_BASEURL_LOCAL,
-  API_BASEURL_PROD,
-} from "../constants/config";
 
 export const contextApi = axios.create({
   baseURL: "https://v1.imezi.com:2000",
--- src/pages/Seller/tools/CekTarif.jsx
+++ src/pages/Seller/tools/CekTarif.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import { Button, Input, Select, Space, Tooltip } from "antd";
 
 const CekTarif = () => {
+  const { TextArea } = Input;
   return (
     <div>
       <h1 style={{ fontSize: 30, fontWeight: "bold" }}>Cek Tarif</h1>
+      <br />
+      <h1>Ekspedisi</h1>
+      <Button danger>JNE</Button>
+      <br />
+      <br />
+
+      <Space direction="vertical" style={{ width: "100%" }}>
+        <h3>Alamat Asal</h3>
+        <TextArea
+          showCount
+          maxLength={200}
+          rows={3}
+          placeholder="Kelurahan / Kota"
+        />
+      </Space>
+      <br />
+      <Space direction="vertical" style={{ width: "100%" }}>
+        <h3>Alamat Tujuan</h3>
+        <TextArea
+          showCount
+          maxLength={200}
+          rows={3}
+          placeholder="Kelurahan / Kota"
+        />
+      </Space>
     </div>
   );
 };
